42442a51c7cff005cbd71b8036858df00dc7e2fe
mishra-riya06 <mriya0619@gmail.com>
2025-10-06 20:46:35 +0530

    more in this  project

--- pra/index.js
+++ pra/index.js
@@ -1,21 +1,41 @@
-let url ="https://catfact.ninja/fact ";
+// let url ="https://catfact.ninja/fact ";
+// let btn = document.querySelector("button");
+
+// btn.addEventListener("click", async()=>{
+//     let fact = await getfact()
+//     console.log(fact);
+// })
+
+
+// async function getfact(){
+//     try{
+//         let res = await axios.get(url);
+//         return res.data.fact;
+//     }
+//     catch(e){{
+//         console.log("error",e)
+//         return "No fact found"
+//     }
+//     }
+// }
+
+// Dog img print
+
+let url ="https://dog.ceo/api/breeds/image/random";
 let btn = document.querySelector("button");
 
 btn.addEventListener("click", async()=>{
-    let fact = await getfact()
-    console.log(fact);
+    let link = await getimg()
+   let img = document.querySelector("#result");
+   img.setAttribute("src",link);
 })
-
-
-async function getfact(){
+async function getimg(){
     try{
         let res = await axios.get(url);
-        return res.data.fact;
+        return res.data.message;
     }
-    catch(e){{
-        console.log("error",e)
-        return "No fact found"
-    }
-
+    catch(e){
+        console.log("error",e);
+        return "/";
     }
 }
