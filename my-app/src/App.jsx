23cf210614422aac04221a0027b04c61630e819a
mishra-riya06 <mriya0619@gmail.com>
2025-10-08 18:19:39 +0530

    First components

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './component/button'
+
+const App = () => {
+  return (
+    <> 
+    <h1>hello world</h1>
+    <Button text = "click me"/>
+   
+    </>
+  )
+}
+
+export default App
